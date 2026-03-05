267bfb751b642172318ea0cccad400b4291f25be
JoseManuelReyes13z <josemanuelreyesmorales13z@hotmail.com>
2026-03-04 22:38:22 -0600

    Segunda tarea de operaciones basicas

---
+++ Tareas/app2.js
@@ -0,0 +1,12 @@
+var numUsuario1 = parseInt(prompt("Dame el primer numero para realizar las operaciones"));
+var numUsuario2 = parseInt(prompt("Dame el segundo numero para realizar las operaciones"));
+
+console.log("La suma del primer mas el segundo numero es "+(numUsuario1+numUsuario2));
+
+console.log("La resta del primer menos el segundo numero es "+(numUsuario1-numUsuario2));
+
+console.log("La multiplicacion del primer por el segundo numero es "+(numUsuario1*numUsuario2));
+
+console.log("La potencia del primer numero a el valor del segundo numero es "+(numUsuario1**numUsuario2));
+
+console.log("el modulo del primer numero dividido entre el segundo es "+(numUsuario1 % numUsuario2));
